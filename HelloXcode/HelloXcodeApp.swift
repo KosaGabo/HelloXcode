c6da0ccb65ceddcc1ada14a83f7f490f7cdc926e
Kosa Gabor <gabo.it@protonmail.com>
2024-12-11 22:54:27 +0100

    Initial Commit

---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by Kósa Gábor on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloXcodeTests/HelloXcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeTests.swift
+//  HelloXcodeTests
+//
+//  Created by Kósa Gábor on 11/12/2024.
+//
+
+import Testing
+@testable import HelloXcode
+
+struct HelloXcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
